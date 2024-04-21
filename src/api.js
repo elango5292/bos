4f5ad2dea7aa14e7207d60ca778f2cdd4b7dfd45
macbook <macbook@MacBook-Air-2.local>
2024-04-21 19:08:05 +0530

    modified axios instance

--- src/api.js
+++ src/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://bus-booking-ruby.vercel.app/api',
   withCredentials: true,
+  responseType: 'json'
 });
 
 export default api;
